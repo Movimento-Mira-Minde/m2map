b6c3e753fa06304ad9dd937d837d6e5df33e0228
Nuno Fernandes <me@nfriacowboy.com>
2023-01-08 01:15:10 +0000

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {j9ojokjoj
+  {
     path: 'map',
     loadChildren: () =>
       import('./map/map-routing.module').then((m) => m.MapRoutingModule),
